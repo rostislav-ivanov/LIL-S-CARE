0402ae9f727fcb95b0fa13fe50655e4909d9e10c
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-02-25 10:29:27 +0200

    Add project files.

---
+++ LilsCareApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LilsCareApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
