ca9fd28cca66790fb38f7a50e38611be9beeaa4c
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-02 09:40:44 +0200

    Remove Header from Layout.cshtml to HeaderComponenet

---
+++ LilsCareApp/Components/HeaderComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LilsCareApp.Components
+{
+    public class HeaderComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View());
+        }
+    }
+}
