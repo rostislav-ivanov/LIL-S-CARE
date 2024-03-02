5662b4ac923f007084657086b9dc89222334dde4
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-02 07:59:21 +0200

    Create  extention methods of IServiceCollection: AddAppServices, AddAppDbContext, AddAppIdentity. Replase them in Program.cs

---
+++ LilsCareApp/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using LilsCareApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddAppServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddAppDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddAppIdentity(this IServiceCollection services)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
