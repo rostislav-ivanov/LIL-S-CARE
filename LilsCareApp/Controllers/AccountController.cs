32c97cd47444effb35153b8f22d2b928a72d7c93
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-03 17:19:12 +0200

    Reapply "Add Header and Footer"
    This reverts commit 7ee5f30eb0042ba637cd5cc20ddaefde2b8e7545.

---
+++ LilsCareApp/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LilsCareApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult MyAccount()
+        {
+            return View();
+        }
+
+        public IActionResult MyAddresses()
+        {
+            return View();
+        }
+
+        public IActionResult MyOrders()
+        {
+            return View();
+        }
+
+        public IActionResult MyWishes()
+        {
+            return View();
+        }
+    }
+}
---
+++ LilsCareApp/Controllers/IngredientsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LilsCareApp.Controllers
+{
+    public class IngredientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
