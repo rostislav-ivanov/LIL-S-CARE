37fbf0f4ab873a4e18dc80226b00acee740a18be
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-03 17:10:14 +0200

    Add Header and Footer

---
+++ LilsCareApp/Controllers/ShippingReturnsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LilsCareApp.Controllers
+{
+    public class ShippingReturnsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
