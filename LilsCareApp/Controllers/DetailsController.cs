e7069df9165e7f4c710913a5e8657a1c46977023
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-08 10:38:00 +0200

    add template for Details View

---
+++ LilsCareApp/Controllers/DetailsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LilsCareApp.Controllers
+{
+    public class DetailsController : BaseController
+    {
+        public IActionResult Index(int id)
+        {
+            return View(id);
+        }
+    }
+}
