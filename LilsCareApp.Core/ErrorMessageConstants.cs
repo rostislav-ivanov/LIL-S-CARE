50e9be7839c939146e3848b2e8d04fc6927948a6
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-12 17:34:43 +0200

    create Ingredients page

--- LilsCareApp.Core/ErrorMessageConstants.cs
+++ LilsCareApp.Core/ErrorMessageConstants.cs
@@ -2,12 +2,13 @@
 {
     public class ErrorMessageConstants
     {
-        public const string Required = "This field {0} is required";
+        public const string Required = "Моля, въведете {0}!";
         public const string StringLength = "The {0} must be at least {2} and at most {1} characters long";
         public const string DurationRange = "The {0} must be between {1} and {2}";
         public const string ConfirmAgreementPrivacyPolicy = "Потвърдете съгласието с политиката за поверителност.";
         public const string EmailAddress = "Невалиден имейл адрес.";
         public const string RatingRange = "Моля, дайте вашата оценка!";
         public const string RequireRating = "Моля, дайте вашата оценка!";
+        public const string PrivacyPolicyCheck = "Моля, потвърдете съгласието с политиката за поверителност.";
     }
 }
--- LilsCareApp.Core/Models/SubscriberDTO.cs
+++ LilsCareApp.Core/Models/SubscriberDTO.cs
@@ -1,8 +1,20 @@
-﻿namespace LilsCareApp.Core.Models
+﻿using LilsCareApp.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using static LilsCareApp.Core.ErrorMessageConstants;
+
+namespace LilsCareApp.Core.Models
 {
     public class SubscriberDTO
     {
-        public required string Email { get; set; }
+        [Required(ErrorMessage = Required)]
+        [DisplayName("имейл")]
+        [EmailAddress(ErrorMessage = EmailAddress)]
+        public required string EmailSubscriber { get; set; }
+
+        [MustBeTrue(ErrorMessage = PrivacyPolicyCheck)]
+        public bool PrivacyPolicyCheckBox { get; set; }
+
         public string? AppUserId { get; set; }
     }
 }
