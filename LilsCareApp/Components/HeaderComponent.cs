1e42499d82f52a60b31fef26916533fed266fe7a
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-21 19:16:17 +0200

    set User Image to Nav Bar

--- LilsCareApp/Components/HeaderComponent.cs
+++ LilsCareApp/Components/HeaderComponent.cs
@@ -6,19 +6,23 @@ namespace LilsCareApp.Components
 {
     public class HeaderComponent : ViewComponent
     {
-        private readonly IProductsService _service;
+        private readonly IProductsService _productsService;
+        private readonly IAccountService _accountService;
+
         private readonly HttpContext _httpContext;
 
 
-        public HeaderComponent(IProductsService service, IHttpContextAccessor httpContextAccessor)
+        public HeaderComponent(IProductsService productsService, IAccountService accountService, IHttpContextAccessor httpContextAccessor)
         {
-            _service = service;
+            _productsService = productsService;
+            _accountService = accountService;
             _httpContext = httpContextAccessor.HttpContext;
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
             string? userId = _httpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
-            ViewBag.count = userId != null ? await _service.GetCountInBagAsync(userId) : 0;
+            ViewBag.Count = userId != null ? await _productsService.GetCountInBagAsync(userId) : 0;
+            ViewBag.UserImagePath = userId != null ? await _accountService.GetUserImagePathAsync(userId) : null;
             return await Task.FromResult((IViewComponentResult)View());
         }
     }
