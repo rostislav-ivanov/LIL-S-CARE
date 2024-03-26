13ff29d4c9008d10cfebf304141494000a74eaaa
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-26 14:24:04 +0200

    Create Guest Service

---
+++ LilsCareApp/Services/IGuestService.cs
@@ -0,0 +1,15 @@
+﻿using LilsCareApp.Core.Models;
+using LilsCareApp.Core.Models.Checkout;
+
+namespace LilsCareApp.Services
+{
+    public interface IGuestService
+    {
+        void AddToCart(int productId, int quantity);
+        void ClearBag();
+        void DeleteProductFromCart(int id);
+        int GetCountInBag();
+        Task<IEnumerable<ProductsInBagDTO>> GetProductsInBagAsync();
+        Task<string> CheckoutSaveAsync(OrderDTO order);
+    }
+}
