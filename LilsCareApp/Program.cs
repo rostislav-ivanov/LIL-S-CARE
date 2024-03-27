930f26a976c331f9d3d4f659dafe43158cb20713
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-27 10:53:27 +0200

    ...

--- LilsCareApp/Program.cs
+++ LilsCareApp/Program.cs
@@ -1,10 +1,24 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
+var supportedCultures = new[]
+{
+    new CultureInfo("en-US"),
+    new CultureInfo("bg-BG")
+};
 
 builder.Services.AddAppDbContext(builder.Configuration);
 builder.Services.AddAppIdentity();
+builder.Services.AddAppLocalization();
+
+builder.Services.AddMvc()
+    .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
 
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddAppServices();
 
 var app = builder.Build();
@@ -25,6 +39,13 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseRequestLocalization(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
 
 // Enable session middleware
 app.UseSession();
