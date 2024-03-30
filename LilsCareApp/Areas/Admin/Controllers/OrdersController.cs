0bc352844c813ee86ec396e8257d4630795ae65f
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-30 15:09:14 +0200

    create Admin / Order and Users Controllers and Views

---
+++ LilsCareApp/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LilsCareApp.Areas.Admin.Controllers
+{
+    public class OrdersController : AdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
