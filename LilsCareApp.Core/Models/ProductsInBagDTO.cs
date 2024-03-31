516b6f9d64eb58de273aca2f0a9b9c448deb617a
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-31 18:50:59 +0300

    Udate DetailDTO and ProductDTO with Optional.

--- LilsCareApp.Core/Models/ProductsInBagDTO.cs
+++ LilsCareApp.Core/Models/ProductsInBagDTO.cs
@@ -5,10 +5,11 @@
         public int Id { get; set; }
         public required string Name { get; set; }
 
-        public string? Weight { get; set; }
+        public KeyValuePair<string, string>? Optional { get; set; }
 
         public decimal Price { get; set; }
         public string? ImageUrl { get; set; }
+
         public int Quantity { get; set; }
     }
 }
