18f644ce4f8443ddfc1d2cac3a1f2b1a3511c5d5
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-03-31 21:06:32 +0300

    Create Admin / Details Product

---
+++ LilsCareApp.Core/Contracts/IAdminDetailsService.cs
@@ -0,0 +1,10 @@
+﻿using LilsCareApp.Core.Models;
+
+namespace LilsCareApp.Core.Contracts
+{
+    public interface IAdminDetailsService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategoriesAsync();
+        Task<DetailsDTO> GetProductByIdAsync(int id);
+    }
+}
