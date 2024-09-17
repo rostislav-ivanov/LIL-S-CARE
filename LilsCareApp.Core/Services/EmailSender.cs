b140a7d2f3a8da10472a2a902ed09ad78b0c3220
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-09-17 18:56:51 +0300

    ...

--- LilsCareApp.Core/Services/EmailSender.cs
+++ LilsCareApp.Core/Services/EmailSender.cs
@@ -12,7 +12,6 @@ namespace LilsCareApp.Core.Services
     {
         private readonly ILogger _logger;
         private readonly IConfiguration _configuration;
-        private readonly string? _sendGridKey;
 
         public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor,
                            ILogger<EmailSender> logger,
@@ -21,18 +20,17 @@ namespace LilsCareApp.Core.Services
             Options = optionsAccessor.Value;
             _logger = logger;
             _configuration = configuration;
-            _sendGridKey = configuration.GetConnectionString("SendGridKey");
         }
 
         public AuthMessageSenderOptions Options { get; }
 
         public async Task SendEmailAsync(string toEmail, string subject, string message)
         {
-            if (string.IsNullOrEmpty(_sendGridKey))
+            if (string.IsNullOrEmpty(Options.SendGridKey))
             {
                 throw new Exception("Null SendGridKey");
             }
-            await Execute(_sendGridKey, subject, message, toEmail);
+            await Execute(Options.SendGridKey, subject, message, toEmail);
         }
 
         public async Task Execute(string apiKey, string subject, string message, string toEmail)
@@ -58,3 +56,67 @@ namespace LilsCareApp.Core.Services
         }
     }
 }
+
+//using LilsCareApp.Core.Configurations;
+//using Microsoft.AspNetCore.Identity.UI.Services;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Options;
+//using SendGrid;
+//using SendGrid.Helpers.Mail;
+
+//namespace LilsCareApp.Core.Services
+//{
+//    public class EmailSender : IEmailSender
+//    {
+//        private readonly ILogger _logger;
+//        private readonly IConfiguration _configuration;
+//        private readonly string? _sendGridKey;
+
+//        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor,
+//                           ILogger<EmailSender> logger,
+//                           IConfiguration configuration)
+//        {
+//            Options = optionsAccessor.Value;
+//            _logger = logger;
+//            _configuration = configuration;
+//            _sendGridKey = configuration.GetConnectionString("SendGridKey");
+//        }
+
+//        public AuthMessageSenderOptions Options { get; }
+
+//        public async Task SendEmailAsync(string toEmail, string subject, string message)
+//        {
+//            if (string.IsNullOrEmpty(_sendGridKey))
+//            {
+//                throw new Exception("Null SendGridKey");
+//            }
+//            await Execute(_sendGridKey, subject, message, toEmail);
+//        }
+
+//        public async Task Execute(string apiKey, string subject, string message, string toEmail)
+//        {
+//            var client = new SendGridClient(apiKey);
+//            string emailAdministrator = _configuration["EmailAdministrator"]!;
+//            var msg = new SendGridMessage()
+//            {
+//                From = new EmailAddress(emailAdministrator, "Lil's Care handmade"),
+//                Subject = subject,
+//                PlainTextContent = message,
+//                HtmlContent = message
+//            };
+//            msg.AddTo(new EmailAddress(toEmail));
+
+//            // Disable click tracking.
+//            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+//            msg.SetClickTracking(false, false);
+//            var response = await client.SendEmailAsync(msg);
+//            _logger.LogInformation(response.IsSuccessStatusCode
+//                                   ? $"Email to {toEmail} queued successfully!"
+//                                   : $"Failure Email to {toEmail}");
+//        }
+//    }
+//}
+
+
+
